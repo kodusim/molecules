3e9f9c9850cb2975bf31585c1b7dd8ee816076f9
Sangki Jeon <sangki1298@gmail.com>
2025-06-15 17:53:46 +0900

    ㅊ

--- config/settings_production.py
+++ config/settings_production.py
@@ -2,12 +2,12 @@ from .settings import *
 import os
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET', SECRET_KEY)
+SECRET_KEY = os.environ.get('DJANGO_SECRET', 'your-default-secret-key-for-production')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']  # 실제 도메인으로 변경 필요
+ALLOWED_HOSTS = ['*']  # CloudType 도메인 허용
 
 # Database
 DATABASES = {
@@ -20,16 +20,28 @@ DATABASES = {
 # Static files
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Whitenoise middleware for serving static files
-MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # 여기에 추가
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
 
-# Security settings
-SECURE_SSL_REDIRECT = False  # HTTPS 사용 시 True로 변경
-SESSION_COOKIE_SECURE = False  # HTTPS 사용 시 True로 변경
-CSRF_COOKIE_SECURE = False  # HTTPS 사용 시 True로 변경
+# Whitenoise 설정
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Security settings (CloudType은 자체 프록시 사용)
+SECURE_SSL_REDIRECT = False
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
+CSRF_TRUSTED_ORIGINS = ['https://*.cloudtype.app']
